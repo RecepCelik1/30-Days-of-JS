9d4c89dcd52a16828474910e5818ef74a4cd8b3f
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-10-02 22:59:03 +0300

    Create variables.js

---
+++ Day-1/variables.js
@@ -0,0 +1,17 @@
+
+/*  
+    1-) string
+    2-) numbers   
+    3-) boolenas (True-False)
+    4-) undefined (tanımsız)
+    5-) null 
+    6-) symbol(sembol)
+
+*/  
+
+    // değişkenleri atamak için let, var, const anahtar kelimelerini kullanırız JS dilinde datatürü otomatik olarak atanır önceden 
+    // belirtmeye gerek yoktur. var değişken türünde çok fazla sızıntı olduğundan dolayı kullanılması pek tavsiye edilmiyor.
+
+let var1 = 10, var2 = "Obi-Wan Kenobi";
+
+console.log(typeof var1);  //==> ayrıca typeof kullanarak bir datanın türünün ne olduğunu ekrana bastırabiliriz.
