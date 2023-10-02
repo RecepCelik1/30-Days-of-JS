3b201869cec0ac22163776e4cbdbb387dceea0c3
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-10-02 22:45:55 +0300

    JS

---
+++ Day-1/day1.js
@@ -0,0 +1,4 @@
+console.log("Hello, world!!");  //console.log() fonksiyonu virgülle ayrılmış birden fazla parametre alabilir. kullanımı şu şekildedir ;
+                                // console.log(param1, param2, param3);
+
+console.log("Welcome", "to", 30 , "days", "of", "JS"); // görüldüğü gibi birden fazla değişken veri türü alabilir.
