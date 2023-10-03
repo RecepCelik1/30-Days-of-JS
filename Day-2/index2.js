f604970c7a566e0a06f765af4414cfaacf909d73
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-10-03 22:51:57 +0300

    Create index2.js

---
+++ Day-2/index2.js
@@ -0,0 +1,40 @@
+//continue of index.js
+
+/*
+    #stringler
+
+*/
+
+/*
+    #string birleştirme 
+    örnek ;
+*/
+
+let firstName = "Recep", space=" ", secondName = "Celik";
+let fullName = firstName + space + secondName;
+console.log(fullName);
+
+// string birleştrimek için toplama sembolünü kullanmak eski bir yöntemdir. Bu yöntem fazla basit ve hataya açıktır. Daha sonra başka
+// bir yöntem göreceğiz.
+
+/* # Uzun değişmez stringler 
+
+string uzunluğu çok büyükse eğer muhtemelen tek bir satıra sığmayacaktır. Stringin bir sonraki satırdan devam edeceğini belirtmek için
+her satırın sonunda ters eğik çizgi kullanabiliriz. (\)
+
+örnek:
+*/
+
+const paragraph = "My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\
+I am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \
+Node.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \
+In the end of 2019, I was thinking to expand my teaching and to reach \
+to global audience and I started a Python challenge from November 20 - December 19.\
+It was one of the most rewarding and inspiring experience.\
+Now, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \
+I hope you are enjoying too."
+
+/* Stringlerdeki kaçış dizileri
+
+
+*/
