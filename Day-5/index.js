574cd642f7e600d0595c6cf51cbc3807e5e49977
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-10-15 23:17:55 +0300

    Create index.js

---
+++ Day-5/index.js
@@ -0,0 +1,7 @@
+                    // Diziler
+
+// tıpkı C dilinde olduğu gibi bu JS tede dizilerin indexi sıfırdan başlar.
+
+// JSte aynı dizi içerisinde farklı türdeki dataları depolayabiliriz ve her bir dizi elemanına index numarası ile erişip sonradan
+// değerini değiştirebilir. Bir dizi boş olabilir.
+
