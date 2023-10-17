8538333a7fb078f1c9cf57bcb4d4ae16a677427a
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-10-17 22:56:14 +0300

    Update forOfLoop.js

--- Day-6/forOfLoop.js
+++ Day-6/forOfLoop.js
@@ -7,7 +7,7 @@ sokardık bunda öyle bişeye gerek yok direk olarak dizinin elemanlarını teke
 içerisine atıyor sonra aşağıdaki kod bloğunda istersek o değişkeni istediğimiz işlemlerden geçirdikten sonra başa dönüp diğer indexe 
 geçiş yapıyor. */
 
-const numbers = [1,2,3,4,5]
+const numbers = [1,2,3,4,5,6,7,8,9]
 
 for (const num of numbers) {
     console.log(num);
@@ -15,3 +15,17 @@ for (const num of numbers) {
 
 // kod pratikliği sağlıyor i tanımlayıp uğraşmak yerine iki satırda hallediyoruz ve daha az karmaşık olduğu için böyle yinelenen 
 //durumlarda oldukça verimlidir. özellikle diziler, haritalar ve dize gibi yapılarda dolaşmak için çok verimlidir.
+
+let sum = 0;
+for (const num1 of numbers){
+
+    sum = sum + num1;
+
+}
+
+console.log(sum);
+
+
+//break ifadesi = C dili ile aynıdır.
+
+//continue ifadesi = C dili ile aynıdır.
