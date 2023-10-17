6bf5d4e1470755c7f2dedf40e10221b52b55e072
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-10-17 14:06:24 +0300

    JS

--- Day-5/index3.js
+++ Day-5/index3.js
@@ -13,3 +13,56 @@ console.log(names.join(' '));       //==> terminaldeki çıktıları inceleyerek
 console.log(names.join(', '));
 console.log(names.join(' # '));
 
+
+
+        /* *Slice metodu */
+
+//slice: bir aralıktaki çoklu elemanları 
+//dilimler iki parametre alır. Bu paramet-
+//reler, başlangıç ve bitiş konumlarıdır. 
+//Bitiş konumu dahil edilmez
+
+console.log(numbers.slice()); //-> bütün ögeyi kopyalar
+
+console.log(numbers.slice(0)); //-> yine bütün ögeyi kopyalar.
+
+console.log(numbers.slice(0, numbers.length)) //-> bütün ögeyi kopyalar
+
+console.log(numbers.slice(1,4)) //-> bu yazılan parametreler index numarasıdır.
+//ilk index dahil edilirken son index dahil edilmez.
+
+
+
+        /* *Dizilerde splice metodu */
+
+//splice: üç tane parametre alır; başlangıç konumu, kaldırılması gereken eleman sayısı ve eklenmesi gereken eleman
+//sayısı.
+
+let splice1 = numbers.splice();
+console.log(splice1); //-> bütün elemanları kaldırı.
+
+const num = [1,2,3,4,5];
+splice1 = num.splice(0,1);
+console.log(splice1); //-> ilk elemanı kaldırır
+
+const num1 = [1,2,3,4,5,6];
+num1.splice(3,3,7,8,9);
+console.log(num1.splice(3,3,7,8,9));
+
+        /* *Push metodu */
+//push : dizinin sonuna eleman ekler.Varolan bir diziye eleman eklemek için push metodunu kullanırız.
+
+const arr = [`item1`,`item2`,`item3`];
+arr.push(`new item`);
+console.log(arr)
+//[`item1`,`item2`,`item3`,`new item`]
+
+
+
+
+
+
+
+
+
+
---
+++ Day-5/index4.js
@@ -0,0 +1,6 @@
+        /* *pop metodu */
+
+//dizinin sonundaki indexi siler.
+let numbers = [1,2,3,4,5];
+let var1 = numbers.pop();
+console.log(var1);
