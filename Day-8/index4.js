5ad9b2aef42a13987bd4c017875e80c5df8800ba
RecepCelik1 <133972905+RecepCelik1@users.noreply.github.com>
2023-10-21 19:22:03 +0300

    Update index4.js

--- Day-8/index4.js
+++ Day-8/index4.js
@@ -1,7 +1,9 @@
     // object için yeni key değerler oluşturma
 
 /* Nesne değişenlerin özelliklerini gösterir yani eşlenebilen ve oluşturulduktan sonra içerisindeki indexleri değiştirebiliriz ve keyler
-ekleyebiliriz. Hadi bi örnek  görelim
+ekleyebiliriz.
+
+methods = Object.assign, Object.keys, Object.values, Object.entries
 */
 
 const person = {
@@ -21,9 +23,21 @@ const person = {
         return `${this.firstName} ${this.surName}`
     },
 }
-person.nationality = `Ethiopian`
+person.nationality = `Turkish`
 person.country = `Sweden`
 person.skills.push(`C`)
 // görüldüğü üzere nesnemize nationality adında yeni bir key eklendi ve country keyi Sweden olarak değiştirildi ve skills arrayine
 //C olarak yeni bir index eklendi.
 console.log(person);
+
+/* Object Methodları
+stringlerde olduğu gibi objectler içinde methodlar vardır hadi görelim
+*/
+
+// object.assign: nesneyi orijinal nesneyi değiştirmeden kopyalamak için kullanılır.
+
+const copyPerson = Object.assign({}, person); //Object.assign için güzel bir örnek
+console.log(copyPerson)
+
+
+
